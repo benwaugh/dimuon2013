9f8a91ca6d73b5400fc92723b4c94e95f8b0d960
Ben Waugh <b.waugh@ucl.ac.uk>
2013-11-07 15:09:17 +0000

    Make into proper module

--- dimuon.py
+++ dimuon.py
@@ -6,10 +6,16 @@ So far just prints numbers of particles to screen.
 
 from ROOT import TFile
 
-file_data = TFile("/home/waugh/data/events.root")
-tree_data = file_data.Get("events")
-n_events = tree_data.GetEntries()
-for i_event in xrange(n_events):
-    tree_data.GetEntry(i_event)
-    n_particles = tree_data.nPart
-    print n_particles
+def tree_from_file(filename):
+    global file_data
+    file_data = TFile(filename)
+    tree_data = file_data.Get("events")
+    return tree_data
+
+if __name__ == '__main__':
+    tree_data = tree_from_file("/home/waugh/data/events.root")   
+    n_events = tree_data.GetEntries()
+    for i_event in xrange(n_events):
+        tree_data.GetEntry(i_event)
+        n_particles = tree_data.nPart
+        print n_particles
