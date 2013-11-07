8f9518c6535c35b91a5c71941d64a505c351178d
Ben Waugh <b.waugh@ucl.ac.uk>
2013-11-07 13:33:40 +0000

    Loop over events and print number of particles

--- dimuon.py
+++ dimuon.py
@@ -4,3 +4,8 @@ from ROOT import TFile
 
 file_data = TFile("/home/waugh/data/events.root")
 tree_data = file_data.Get("events")
+n_events = tree_data.GetEntries()
+for i_event in xrange(n_events):
+    tree_data.GetEntry(i_event)
+    n_particles = tree_data.nPart
+    print n_particles
