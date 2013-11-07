c196d051901122bb04b3fe31f5c5cdfdd9bd408c
Ben Waugh <b.waugh@ucl.ac.uk>
2013-11-07 15:21:54 +0000

    Create histogram (empty)

--- dimuon.py
+++ dimuon.py
@@ -4,7 +4,7 @@ Read events and create histogram of dimuon masses
 So far just prints numbers of particles to screen.
 '''
 
-from ROOT import TFile
+from ROOT import TFile, TH1F
 
 def tree_from_file(filename):
     '''
@@ -15,6 +15,10 @@ def tree_from_file(filename):
     tree_data = file_data.Get("events")
     return tree_data
 
+def dimuon_mass_histo(tree):
+    hist = TH1F()
+    return hist
+
 if __name__ == '__main__':
     tree_data = tree_from_file("/home/waugh/data/events.root")   
     n_events = tree_data.GetEntries()
@@ -22,3 +26,6 @@ if __name__ == '__main__':
         tree_data.GetEntry(i_event)
         n_particles = tree_data.nPart
         print n_particles
+    hist = dimuon_mass_histo(tree_data)
+    hist.Draw()
+    raw_input("Press return to exit")
